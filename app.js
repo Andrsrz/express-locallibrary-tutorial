d73fe479f2530f924c3f346c3a9d40e72b176ca2
Andres Ruiz <andrsruiz@gmail.com>
2020-07-01 10:29:49 -0500

    Use Env Variable

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 // MongoDB
 var mongoose = require('mongoose');
-var mongoDB = "mongodb+srv://dbArdc:Od7jCXXzSomlymIgQEI@cluster0.nayhn.mongodb.net/local_library?retryWrites=true&w=majority";
+var mongoDB = process.env.MONGODB_LOCALLIBRARY;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
