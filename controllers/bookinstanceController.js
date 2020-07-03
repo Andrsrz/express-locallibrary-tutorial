852115e586f58b6b2cdeb14cf683acdd0b6f548f
Andres Ruiz <andrsruiz@gmail.com>
2020-07-03 16:03:16 -0500

    Implement Delete BookInstance Method

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -94,13 +94,31 @@ exports.bookinstance_create_post = [
 ];
 
 /* Display BookInstance delete form on GET */
-exports.bookinstance_delete_get = (req, res) => {
-	res.send('NOT IMPLEMENTED: BookInstance delete GET');
+exports.bookinstance_delete_get = (req, res, next) => {
+	BookInstance.findById(req.params.id)
+				.populate('book')
+				.exec((err, bookinstance) => {
+		if(err)
+			return next(err);
+
+		if(bookinstance == null)
+			res.redirect('/catalog/bookinstances');
+
+		/* Success */
+		res.render('bookinstance_delete', { title: 'Delete BookInstance',
+											bookinstance: bookinstance });
+	});
 };
 
 /* Handle BookInstance delete on POST */
-exports.bookinstance_delete_post = (req, res) => {
-	res.send('NOT IMPLEMENTED: BookInstance delete POST');
+exports.bookinstance_delete_post = (req, res, next) => {
+	BookInstance.findByIdAndRemove(req.body.bookinstanceid, (err) => {
+		if(err)
+			return next(err);
+
+		/* Success */
+		res.redirect('/catalog/bookinstances');
+	});
 };
 
 /* Display BookInstance update form on GET */
